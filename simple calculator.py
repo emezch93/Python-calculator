db710e25d7ab5d576f76be04e1a460b5bcf252d9
emezch93 <emezch93@gmail.com>
2025-07-18 21:30:34 +0100

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,20 @@
+print("What operation do you want?")
+operator = input("Enter either +, -, * or /: ")
+
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+
+if operator == '+':
+  print(n1, operator, n2, "=", n1+n2)
+
+elif operator == '-':
+  print(n1, operator, n2, "=", n1-n2)
+
+elif operator == '*':
+  print(n1, operator, n2, "=", n1*n2)
+
+elif operator == '/':
+  print(n1, operator, n2, "=", n1/n2)
+
+else:
+  print("Invalid operator")
